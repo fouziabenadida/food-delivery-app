32df50359c9bd735e904c87c97d463274785d635
benadidafouzia <benadidafouzia@gmail.com>
2024-08-21 09:33:28 +0300

    some changes about responsive things

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
+import RestaurantPage from "./pages/restaurantPage/RestaurantPage";
+import CartPage from "./pages/cartPage/CartPage";
+import CheckoutPage from "./pages/checkoutPage/CheckoutPage";
+import Footer from "./components/footer/Footer";
 import "./styles/variables.css";
 
 const App: React.FC = () => {
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/restaurant/:id"
+          element={
+            <RestaurantPage
+              id={0}
+              name={""}
+              image={""}
+              cuisine={""}
+              rating={0}
+              description={""}
+            />
+          }
+        />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,5 +1,8 @@
+// src/pages/homePage/HomePage.tsx
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import Header from "../../components/header/Header";
 import RestaurantCard from "../../components/restaurantCard/RestaurantCard";
 import Footer from "../../components/footer/Footer";
@@ -17,6 +20,7 @@ const HomePage: React.FC = () => {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchRestaurants = async () => {
@@ -34,7 +38,7 @@ const HomePage: React.FC = () => {
   }, []);
 
   if (loading) {
-    <div className="spinner"></div>;
+    return <div className="spinner"></div>;
   }
 
   if (error) {
@@ -47,8 +51,6 @@ const HomePage: React.FC = () => {
       <main className="main-content">
         <h1 className="page-title">Restaurants</h1>
         <div className="restaurant-cards">
-          {" "}
-          {/* Updated class name */}
           {restaurants.map((restaurant) => (
             <RestaurantCard
               key={restaurant.id}
@@ -56,7 +58,7 @@ const HomePage: React.FC = () => {
               image={restaurant.image}
               cuisine={restaurant.cuisine}
               rating={restaurant.rating}
-              onClick={() => alert(`Clicked on ${restaurant.name}`)}
+              onClick={() => navigate(`/restaurant/${restaurant.id}`)}
             />
           ))}
         </div>
